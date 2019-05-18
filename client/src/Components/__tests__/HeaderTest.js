24e008a08388294a4ba2181806f4fe58a142f566
Harika-Raj <r121662@rguktrkv.ac.in>
2019-05-18 16:06:43 +0530

    Add nginx Load Balancer

--- client/src/Components/__tests__/HeaderTest.js
+++ client/src/Components/__tests__/HeaderTest.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../Header'
-import renderer from 'react-test-renderer';
+import Header from './Header'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme';
 
-test('Renders header title component',() =>{
-    const component = renderer.create(
-        <Header/> ,);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
+describe('Renders header title component',() =>{
+    it('It should render the Title',() => {
+        const component = shallow(<Header><h1 id="unique"/></Header>)
+        expect(component.contains(<h1 id="unique"/>)).to.equal(true)
+    })
 })
