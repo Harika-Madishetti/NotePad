d02e1cb2f8b1b74048b2f737f6a95a15fecac21e
Harika-Raj <r121662@rguktrkv.ac.in>
2019-05-19 10:56:07 +0530

    Add setup for shallow and enzyme

--- client/src/Components/__tests__/NoteFormTest.js
+++ client/src/Components/__tests__/NoteFormTest.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import renderer from "react-test-renderer";
-import NoteForm from "../NoteForm";
+import NoteForm from "./NoteForm";
 
 test('Renders NoteForm',() =>{
     const component = renderer.create(
         <NoteForm/> ,);
     let Form = component.toJSON();
     expect(Form).toMatchSnapshot();
-})
+});
