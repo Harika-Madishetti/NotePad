fb2d85b53110fd9d47d81983f1be5b0a3fd01cb1
Harika-Raj <r121662@rguktrkv.ac.in>
2019-05-19 10:57:37 +0530

    Add test cases for Header Component

--- client/src/Components/__tests__/HeaderTest.js
+++ client/src/Components/__tests__/HeaderTest.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Header from './Header'
-import renderer from 'react-test-renderer'
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import Header from '../Header';
 
-describe('Renders header title component',() =>{
-    it('It should render the Title',() => {
-        const component = shallow(<Header><h1 id="unique"/></Header>)
-        expect(component.contains(<h1 id="unique"/>)).to.equal(true)
+describe("component", () => {
+    const Wrapper = shallow(<Header/>)
+    const component = mount(<Header/>)
+    it("should render Header Component", () => {
+        expect(component.getElements()).toMatchSnapshot();
+    });
+
+    it("should check exitance of tag header", () => {
+        expect(component.find('header')).toExist();
+    })
+
+    it("should check exitance of tag h1", () => {
+        expect(component.find('h1')).toExist();
+    })
+
+    it("should check the text in tag h1", () => {
+        expect(component.find('h1')).toHaveText('NotePad')
     })
 })
